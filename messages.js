237bb9106d7ff061c740ee515a1b0c44a95f5767
Mike Eason <mike.eason@mail.com>
2017-10-01 10:29:24 +0100

    Forgot to actually include the tests and added a way to extract parts of comments into bot responses

---
+++ messages.js
@@ -0,0 +1,92 @@
+const responses = require('./responses.json');
+
+const groupMatchRegex = /\$(\d*)/gi;
+
+function extractMessage(comment, resp) {
+    let regex = new RegExp(resp.pattern, 'gi');
+    let matches = regex.exec(comment.body);
+    let message = null;
+
+    if (matches && matches.length > 0) {
+        //If there is an ignore pattern, execute that and check
+        //if we need to ignore this comment.
+        if (resp.ignorePattern) {
+            let ignoreRegex = new RegExp(resp.ignorePattern, 'gi');
+            let ignoreMatches = ignoreRegex.exec(comment.body);
+
+            if (ignoreMatches && ignoreMatches.length > 0)
+                return null;
+        }
+
+        //if we get to here then we can extract a response. 
+        //Pick a random response to send back.
+        if (resp.responses)
+            message = resp.responses[Math.floor(Math.random() * resp.responses.length)];
+        else
+            message = resp.response;
+
+        //Check if the message contains a group match keyword, i.e $0, $1 ect.
+        //A $ symbol followed by a number indicates the matching group to add to the text.
+        let groupMatch = groupMatchRegex.exec(message);
+
+        //Go through each match and extract the group from the original message.
+        while(groupMatch != null) {
+            let identifier = groupMatch[0];
+            let index = parseInt(groupMatch[1]);
+
+            if (Number.isNaN(index))
+                break;
+
+            let origGroup = matches[index + 1].trim();
+
+            message = message.replace(identifier, origGroup);
+
+            groupMatch = groupMatchRegex.exec(message);
+        }
+    }
+
+    //Check if the message contains any keywords.
+    if (message && message.indexOf('$username') > -1) {
+        message = message.replace('$username', comment.author.name);
+    }
+
+    return message;
+}
+
+module.exports = {
+
+    extractReply(comment, prevCommentIds = []) {
+        //make sure we're not replying to ourselves.
+        if (comment.author.name === process.env.REDDIT_USER)
+            return null;
+    
+        let message;
+        
+        if (prevCommentIds.includes(comment.parent_id)) {
+            //This comment is a reply to one of ours, check for a reply.
+            for(let i = 0; i < responses.replies.length; i++) {
+                let resp = responses.replies[i];
+                message = extractMessage(comment, resp);
+    
+                if (message)
+                    return message;
+            }
+
+            //Always exit out if the comment is a reply to one of ours.
+            return null;
+        }
+    
+        //if we get to here then check if the comment contains one of
+        //our key phrases and send back a response.
+        for(let i = 0; i < responses.messages.length; i++) {
+            let resp = responses.messages[i];
+            let message = extractMessage(comment, resp);
+    
+            if (message)
+                return message;
+        }
+    
+        return null;
+    }
+
+};
---
+++ spec/messagesSpec.js
@@ -0,0 +1,146 @@
+require('dotenv').config();
+const messages = require('../messages');
+
+describe('messages and responses', () => {
+
+    it('should return null if no matches', () => {
+        let comment = {
+            body: 'This is not a quote from the prequels.',
+            author: {
+                name: 'user_123456789'
+            }
+        };
+
+        let message = messages.extractReply(comment);
+
+        expect(message).toBeNull();
+    });
+
+    it('should return a message if there are matches', () => {
+        let comment = {
+            body: `it's over anakin, i have the high ground!`,
+            author: {
+                name: 'user_123456789'
+            }
+        };
+
+        let message = messages.extractReply(comment);
+
+        expect(message).not.toBeNull();
+    });
+    
+    it('should not care about case sensitivity', () => {
+        let comment = {
+            body: `IT'S OVER ANAKIN, I HAVE THE HIGH GROUND!`,
+            author: {
+                name: 'user_123456789'
+            }
+        };
+
+        let message = messages.extractReply(comment);
+
+        expect(message).not.toBeNull();
+    });
+
+    it('should contain the username if the match contains a $username keyword', () => {
+        let comment = {
+            body: 'All I want is your love.',
+            author: {
+                name: 'user_123456789'
+            }
+        };
+
+        let message = messages.extractReply(comment);
+
+        expect(message).not.toBeNull();
+        expect(message).toContain(comment.author.name);
+    });
+
+    it('should check the ignore pattern', () => {
+        let comment = {
+            body: `The Jedi are evil!`,
+            author: {
+                name: 'user_123456789'
+            }
+        };
+
+        let message = messages.extractReply(comment);
+
+        expect(message).toBeNull();
+    });
+    
+    it('should reply to a user if good bot is mentioned.', () => {
+        let comment = {
+            parent_id: '123',
+            body: `Good bot.`,
+            author: {
+                name: 'user_123456789'
+            }
+        };
+
+        let prevCommentIds = [comment.parent_id];
+
+        let message = messages.extractReply(comment, prevCommentIds);
+
+        expect(message).not.toBeNull();
+    });
+    
+    it('should reply to a user if bad bot is mentioned.', () => {
+        let comment = {
+            parent_id: '123',
+            body: `Bad bot.`,
+            author: {
+                name: 'user_123456789'
+            }
+        };
+
+        let prevCommentIds = [comment.parent_id];
+
+        let message = messages.extractReply(comment, prevCommentIds);
+
+        expect(message).not.toBeNull();
+    });
+    
+    it('should not reply to a user if neither good bot or bad bot is mentioned.', () => {
+        let comment = {
+            parent_id: '123',
+            body: `It's over Anakin, I have the high ground!`,
+            author: {
+                name: 'user_123456789'
+            }
+        };
+
+        let prevCommentIds = [comment.parent_id];
+
+        let message = messages.extractReply(comment, prevCommentIds);
+
+        expect(message).toBeNull();
+    });
+
+    it('should not reply to itself', () => {
+        let comment = {
+            body: `It's over Anakin, I have the high ground!`,
+            author: {
+                name: process.env.REDDIT_USER
+            }
+        };
+
+        let message = messages.extractReply(comment);
+
+        expect(message).toBeNull();
+    });
+
+    it('should replace group match keywords with text contained in the comment', () => {
+        let comment = {
+            body: `The chancellor was right, you've changed.`,
+            author: {
+                name: 'user_123456789'
+            }
+        };
+
+        let message = messages.extractReply(comment);
+
+        expect(message).not.toBeNull();
+        expect(message).toContain('The chancellor');
+    });
+});
